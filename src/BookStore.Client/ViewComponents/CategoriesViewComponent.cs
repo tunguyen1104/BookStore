bd6f8175795d8dc3221009a898362e55c18bbb49
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-19 16:58:37 +0700

    feat: dynamically load categories

---
+++ src/BookStore.Client/ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Application.Services;
+using BookStore.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Client.ViewComponents
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly IService<Category> _categoryService;
+
+        public CategoriesViewComponent(IService<Category> categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Category> categories = await _categoryService.GetAllAsync();
+            return View(categories);
+        }
+    }
+}
