2d6dac86e67e37f0c4e653c4bfd48c0991c3d851
Giappp <103166317+Giappp@users.noreply.github.com>
2024-10-20 17:18:50 +0700

    feat: Books view for admin

---
+++ src/BookStore.Admin/Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Admin.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult BookList()
+        {
+            return View();
+        }
+
+    }
+}
