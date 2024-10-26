cc5bac244080f816ad98efef840121072094cf10
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-26 21:29:45 +0700

    add css and js file

---
+++ src/BookStore.Client/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Client.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
