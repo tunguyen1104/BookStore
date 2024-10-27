128f30e1f0fa3893d62b6f30ee10d4ba76543d8f
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-27 20:21:22 +0700

    feat: Add Custom Authorize Attribute

---
+++ src/BookStore.Application/Config/CustomAuthorizeAttribute.cs
@@ -0,0 +1,47 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Routing;
+using BookStore.Domain.Enums;
+using Newtonsoft.Json;
+
+namespace BookStore.Application.Config
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class CustomAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        private const string LoginController = "Auth";
+        private const string LoginAction = "Login";
+        private const string AccessDeniedAction = "AccessDenied";
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var userDataClaim = context.HttpContext.User.FindFirst(ClaimTypes.UserData)?.Value;
+
+            if (string.IsNullOrEmpty(userDataClaim))
+            {
+                RedirectTo(context, LoginController, LoginAction);
+                return;
+            }
+            var userConfig = JsonConvert.DeserializeObject<User>(userDataClaim);
+            if (userConfig == null || !IsAuthorizedRole(userConfig.RoleId))
+            {
+                RedirectTo(context, LoginController, AccessDeniedAction);
+            }
+        }
+
+        private static bool IsAuthorizedRole(long roleType)
+        {
+            return roleType == (long)RoleEnum.ADMIN;
+        }
+
+        private static void RedirectTo(AuthorizationFilterContext context, string controller, string action)
+        {
+            context.Result = new RedirectToRouteResult(
+                new RouteValueDictionary { { "controller", controller }, { "action", action } }
+            );
+        }
+    }
+}
