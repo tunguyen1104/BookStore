be1e43d8476fa43cf18c29f46ce3816942b6407f
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-27 12:54:46 +0700

    feat: configure cookie authentication and add HttpContextAccessor in Program.cs

--- src/BookStore.Client/Program.cs
+++ src/BookStore.Client/Program.cs
@@ -4,7 +4,9 @@ using BookStore.Application.Services.Impl;
 using BookStore.Domain.Repositories;
 using BookStore.Infrastructure.Data;
 using BookStore.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+
 namespace BookStore.Client
 {
     public class Program
@@ -24,7 +26,19 @@ namespace BookStore.Client
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddHttpContextAccessor();
             builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.LoginPath = "/auth/login";
+                options.SlidingExpiration = true;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
