47f5a3335bff94b0cd298251e794118c8e14b62f
TuNguyen1104 <tu.pro5564@gmail.com>
2024-10-27 20:25:04 +0700

    feat: Redirect admins to the admin dashboard

--- src/BookStore.Client/Controllers/AuthController.cs
+++ src/BookStore.Client/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ namespace BookStore.Client.Controllers
     public class AuthController : Controller
     {
         private readonly IUserService _userService;
+        private const string AdminDashboardUrl = "http://localhost:5118/";
         public AuthController(IUserService userService)
         {
             _userService = userService;
@@ -67,6 +68,7 @@ namespace BookStore.Client.Controllers
             }
             else
             {
+                if(_userService.IsAuthorizedRole(0)) return Redirect(AdminDashboardUrl);
                 return RedirectToAction("Index", "Home");
             }
         }
