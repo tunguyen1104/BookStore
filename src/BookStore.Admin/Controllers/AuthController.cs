49388aea4056152b12f9ccd9acb9e9519830cd6e
Giappp <103166317+Giappp@users.noreply.github.com>
2024-10-29 11:48:01 +0700

    feat: Admin Auth

---
+++ src/BookStore.Admin/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
