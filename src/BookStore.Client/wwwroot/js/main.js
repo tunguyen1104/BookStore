7cafa70ea8fb6d34c97000087c6793549e09c99b
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-02 22:28:36 +0700

    Remove default quantity increment and decrement functionality in shopping cart

--- src/BookStore.Client/wwwroot/js/main.js
+++ src/BookStore.Client/wwwroot/js/main.js
@@ -205,21 +205,6 @@
     var proQty = $('.pro-qty');
     proQty.prepend('<span class="dec qtybtn">-</span>');
     proQty.append('<span class="inc qtybtn">+</span>');
-    proQty.on('click', '.qtybtn', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
-    });
     $(document).ready(function () {
         $('#userIcon').click(function (event) {
             event.preventDefault();
