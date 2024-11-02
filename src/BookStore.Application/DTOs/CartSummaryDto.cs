d778799c17db00ea168385821fcd6b62c800ac1a
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-02 21:19:41 +0700

    Add CartSummaryDto class to represent summary details of a shopping cart

---
+++ src/BookStore.Application/DTOs/CartSummaryDto.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Domain.Entities;
+
+namespace BookStore.Application.DTOs
+{
+    public class CartSummaryDto
+    {
+        public double TotalPrice { get; set; }
+        public double TotalDiscount { get; set; }
+        public List<CartDetail> CartDetails { get; set; }
+        public CartSummaryDto()
+        {
+            CartDetails = new List<CartDetail>();
+        }
+        public CartSummaryDto(double totalPrice, double totalDiscount, List<CartDetail> cartDetails)
+        {
+            TotalPrice = totalPrice;
+            TotalDiscount = totalDiscount;
+            CartDetails = cartDetails;
+        }
+    }
+}
