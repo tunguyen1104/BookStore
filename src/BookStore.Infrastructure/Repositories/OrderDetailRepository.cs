bb6e5a063b10bc96090e437718a48ae875338e4e
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-03 22:22:39 +0700

    feat: Add Order and OrderDetail repository

---
+++ src/BookStore.Infrastructure/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Domain.Entities;
+using BookStore.Domain.Repositories;
+using BookStore.Infrastructure.Data;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(BookStoreDbContext context) : base(context)
+        {
+        }
+    }
+}
