51e77b54d30f90cbb6317a34cb69dddffc0ed400
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-03 16:21:32 +0700

    refactor: Extract CartDetailDto for cleaner architecture

---
+++ src/BookStore.Application/DTOs/CartDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Application.DTOs
+{
+    public class CartDetailDto
+    {
+        public long Id { get; set; }
+        public long BookId { get; set; }
+        public decimal BookPrice { get; set; }
+        public long Quantity { get; set; }
+        public string BookName { get; set; } = null!;
+        public string BookImage { get; set; } = null!;
+        public decimal BookDiscount { get; set; }
+    }
+}
