74c0fbe7cd1670a54df2a534f5a9d25ecc857852
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-03 20:19:23 +0700

    Add CheckoutDto classes for handling checkout data

---
+++ src/BookStore.Application/DTOs/CheckoutDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.DTOs
+{
+    public class CheckoutDto
+    {
+        public CartSummaryDto? CartSummary { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
+        public string ReceivedName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string ReceivedPhone { get; set; } = null!;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string ReceivedAddress { get; set; } = null!;
+        public string? OrderNotes { get; set; }
+    }
+}
