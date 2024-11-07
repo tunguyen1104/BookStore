b8337b14377628ef015d3140b3eb2bd20416ad04
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-07 13:56:53 +0700

    Add DTO to display new arrival and best seller books on homepage

---
+++ src/BookStore.Application/DTOs/HomePageBookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Application.DTOs
+{
+    public class HomePageBookDto
+    {
+        public BookDto BookDto { get; set; } = null!;
+        public bool IsNewArrival { get; set; }
+        public bool BestSeller { get; set; }
+    }
+}
