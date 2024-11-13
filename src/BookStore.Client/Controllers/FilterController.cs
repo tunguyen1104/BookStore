c68c47c13d2f196b0a541c5bb714f621f0904471
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-13 17:10:45 +0700

    feat: Add API endpoints to load genres and factories data

---
+++ src/BookStore.Client/Controllers/FilterController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Application.Services;
+using BookStore.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Client.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilterController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IUnitOfWork _supplierRepository;
+        public FilterController(ICategoryService categoryService, IUnitOfWork supplierRepository)
+        {
+            _categoryService = categoryService;
+            _supplierRepository = supplierRepository;
+        }
+        [HttpGet("genres")]
+        public async Task<IActionResult> GetGenres()
+        {
+            var genres = await _categoryService.GetAllAsync();
+            return Ok(genres);
+        }
+        [HttpGet("factories")]
+        public async Task<IActionResult> GetFactory()
+        {
+            var factories = await _supplierRepository.Suppliers.GetAllAsync();
+            return Ok(factories);
+        }
+    }
+}
+
+
