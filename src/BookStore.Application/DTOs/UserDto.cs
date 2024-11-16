3bd966d0796b7876971cc29466970fb339ff2c3f
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-16 11:36:02 +0700

    Add UserDto class for data transfer

---
+++ src/BookStore.Application/DTOs/UserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Application.DTOs
+{
+    public class UserDto
+    {
+        public long Id { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", ErrorMessage = "Email is not valid")]
+        public string Email { get; set; } = null!;
+
+        public string? Avatar { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
+        public string? FullName { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string? Address { get; set; }
+    }
+}
