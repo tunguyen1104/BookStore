9b9682a5a99974794cd3c18e34220e2c449e2f6b
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-16 11:54:57 +0700

    Add Razor page for My Profile and routing

---
+++ src/BookStore.Client/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.Application.DTOs;
+using BookStore.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserService _userService;
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Route("my-profile")]
+        public IActionResult MyProfile()
+        {
+            var user = _userService.GetCurrentUser();
+
+            var userDto = new UserDto
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Avatar = user.Avatar,
+                Address = user.Address,
+                Phone = user.Phone,
+                Email = user.Email
+            };
+
+            return View(userDto);
+        }
+
+    }
+}
