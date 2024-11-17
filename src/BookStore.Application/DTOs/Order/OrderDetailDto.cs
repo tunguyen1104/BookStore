3d64b156a98ee7140f6f4d9a5462e1bee4c1d0d4
TuNguyen1104 <tu.pro5564@gmail.com>
2024-11-17 18:59:11 +0700

    feat: add DTOs and services for order history management

---
+++ src/BookStore.Application/DTOs/Order/OrderDetailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Application.DTOs.Order
+{
+    public class OrderDetailDto
+    {
+        public string BookName { get; set; } = null!;
+
+        public string BookImage { get; set; } = null!;
+
+        public decimal BookPrice { get; set; }
+
+        public long Quantity { get; set; }
+    }
+}
---
+++ src/BookStore.Application/DTOs/Order/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Application.DTOs.Order
+{
+    public class OrderDto
+    {
+        public long OrderId { get; set; }
+
+        public string ReceivedName { get; set; } = null!;
+
+        public string ReceivedPhone { get; set; } = null!;
+
+        public string ReceivedAddress { get; set; } = null!;
+
+        public decimal TotalPrice { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+
+        public string OrderNotes { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
+    }
+}
