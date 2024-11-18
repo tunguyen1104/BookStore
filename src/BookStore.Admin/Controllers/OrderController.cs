2322072b6c2fdaca08cc06254c60cdc1305ecde2
Giappp <103166317+Giappp@users.noreply.github.com>
2024-11-18 21:11:38 +0700

    feat: Add order list and improve UI admin site

--- src/BookStore.Admin/Controllers/OrderController.cs
+++ src/BookStore.Admin/Controllers/OrderController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Admin.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public IActionResult OrderManagement()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult GetOrders(int draw, string searchValue, int start, int length)
+        {
+            int page = (start / length) + 1;
+            var (orderDtos, filterCount, totalCount) = _orderService.Find(searchValue, page, length);
+            return Json(new
+            {
+                draw = draw,
+                recordsTotal = totalCount,
+                recordsFiltered = filterCount,
+                data = orderDtos
+            });
+        }
+        [HttpGet("Order/GetOrderDetailAsync")]
+        public async Task<IActionResult> GetOrderDetailAsync(long orderId)
+        {
+            var orderDetailDto = await _orderService.GetOrderDetailsByOrderIdAsync(orderId);
+            if (orderDetailDto == null || !orderDetailDto.Any())
+            {
+                return Json(new { success = false, message = "Order details not found" });
+            }
+
+            return Json(new { success = true, data = orderDetailDto });
         }
     }
 }
